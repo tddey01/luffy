1d4fc018a17aa5045a4b4a1b18fb1f1bc8481660
access <kevinlandun@gmail.com>
2020-06-10 13:16:02 +0800

    if判断
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ day001/if/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// age := 19
+	// if age > 18 {
+	// 	fmt.Println("澳门线上赌城开业了")
+	// } else if age < 18 {
+	// 	fmt.Println("Waring....")
+	// } else {
+	// 	fmt.Println("回去等长大些再来")
+	// }
+	age := 18
+	if age == 18 {
+		fmt.Println("长大了")
+	}
+
+	if age2 := 28; age2 > 19 {
+		fmt.Println("长大了")
+	}
+
+}
