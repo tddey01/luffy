c5a551289efef80ef1b9d9015c3641cb522609a8
access <kevinlandun@gmail.com>
2020-06-11 13:04:05 +0800

    运算符
    Signed-off-by: access <kevinlandun@gmail.com>

--- day002/01operators/main.go
+++ day002/01operators/main.go
@@ -27,14 +27,29 @@ func main() {
 	// fmt.Println(n1 < n2)
 	// fmt.Println(n1 <= n2)
 
-	//逻辑运算符
-	a := true
-	b := false
-	// 两个条件都成立为真
-	fmt.Println(a && b)
-	// 逻辑 OR 运算符。 如果两边的操作数有一个 True，则为 True，否则为 False。 两个条件有一个为真
-	fmt.Println(a || b)
-	// 逻辑 NOT 运算符。 如果条件为 True，则为 False，否则为 True。  原来取费 就为假
-	fmt.Println(!a) //false
-	fmt.Println(!b) //true
+	// //逻辑运算符
+	// a := true
+	// b := false
+	// // 两个条件都成立为真
+	// fmt.Println(a && b)
+	// // 逻辑 OR 运算符。 如果两边的操作数有一个 True，则为 True，否则为 False。 两个条件有一个为真
+	// fmt.Println(a || b)
+	// // 逻辑 NOT 运算符。 如果条件为 True，则为 False，否则为 True。  原来取费 就为假
+	// fmt.Println(!a) //false
+	// fmt.Println(!b) //true
+	// 位运算
+	// 1101 13 11 3
+	fmt.Printf("13的二进制%b\n", 13) //1101
+	fmt.Printf("3的二进制%b\n", 3)   //11
+	//  & (两个对应的二进制位都为1才为1)
+	fmt.Println(13 & 3) // 返回1
+	//  | 两两个对应的二进制位有一位为1 就唯1
+	fmt.Println(13 | 3)
+	//  ^ (两个对应的二进制位不一样则为1)
+	fmt.Println(13 ^ 3)
+	// <<
+	fmt.Println(3 << 10)
+	//  >>
+	fmt.Println(3 >> 1)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/tddey01/luffy
 
-go 1.14
+go 1.14j
