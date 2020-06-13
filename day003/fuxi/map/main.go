c3fe5ff2aa1aa5269a27a90ef71dbdaa2b7672db
access <kevinlandun@gmail.com>
2020-06-13 16:31:52 +0800

    map if判断
    Signed-off-by: access <kevinlandun@gmail.com>

--- day003/fuxi/map/main.go
+++ day003/fuxi/map/main.go
@@ -8,4 +8,11 @@ func main() {
 	n1 = make(map[string]int, 3) // 初花map
 	n1["沙河"] = 1
 	fmt.Println(n1)
+	v, ok := n1["上海"]
+	if ok {
+		fmt.Println("n1没有上海")
+		fmt.Println(v)
+	} else {
+		fmt.Println("n1没有上海这个key")
+	}
 }
