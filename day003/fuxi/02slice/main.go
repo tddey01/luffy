d5ef39ed6e91433159766d3bb9dbaae9ef365566
access <kevinlandun@gmail.com>
2020-06-13 15:53:47 +0800

    复习切片
    Signed-off-by: access <kevinlandun@gmail.com>

--- day003/fuxi/02slice/main.go
+++ day003/fuxi/02slice/main.go
@@ -25,26 +25,33 @@ func main() {
 	//     第一个元素在底层数组的位置
 	//     大小(len) 指的是当前切片中元素的数量
 	//     容量（cap)   指的底层数组能容纳的元素的个数
-	var x = []int{1, 2, 3} //【1 2 3】
-	fmt.Println(x)
-	x2 := x[0:2]
-	fmt.Println(x2)
+	// var x = []int{1, 2, 3} //【1 2 3】
+	// fmt.Println(x)
+	// x2 := x[0:2]
+	// fmt.Println(x2)
 
-	// 2 由一个数组切片得到
-	var a = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8}
-	s1 := a[0:len(a)] // a[:]
-	fmt.Println(s1)
-	s2 := a[0:4] // 从开始一直切到3
-	s3 := a[4:]  // 从索引为4一直切到最后
-	fmt.Println(s2)
-	fmt.Println(s3)
+	// // 2 由一个数组切片得到
+	// var a = [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	// s1 := a[0:len(a)] // a[:]
+	// fmt.Println(s1)
+	// s2 := a[0:4] // 从开始一直切到3
+	// s3 := a[4:]  // 从索引为4一直切到最后
+	// fmt.Println(s2)
+	// fmt.Println(s3)
 
-	//  3 由切片在切片得到
-	s4 := s2[:]
+	// //  3 由切片在切片得到
+	// s4 := s2[:]
 
-	fmt.Println(s4)
-	// 4 make()创建切片
-	s5 := make([]bool, 2, 10)
-	fmt.Println(s5)
+	// fmt.Println(s4)
+	// // 4 make()创建切片
+	// s5 := make([]bool, 2, 10)
+	// fmt.Println(s5)
+
+	//  切片追加元素
+	a := []int{1, 2, 3}
+	fmt.Println(len(a), cap(a))
+	a = append(a, 4)
+	fmt.Println(a)
+	fmt.Println(len(a), cap(a))
 
 }
