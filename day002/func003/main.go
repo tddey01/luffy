972ee7d03036838de3bf8715cec7eebdf2e674f2
access <kevinlandun@gmail.com>
2020-06-13 12:43:03 +0800

    闭包函数和匿名函数
    Signed-off-by: access <kevinlandun@gmail.com>

--- day002/func003/main.go
+++ day002/func003/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // 匿名函数和闭包
 
@@ -13,12 +16,43 @@ func a(name string) func() {
 	}
 }
 
+func makeSuffixFunc(suffix string) func(string) string {
+	return func(name string) string {
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+		}
+		return name
+	}
+}
+
+func calc(base int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		return base
+	}
+	sub := func(i int) int {
+		base -= 1
+		return base
+	}
+	return add, sub
+}
+
 func main() {
 	// func() {
 	// 	fmt.Println("匿名函数")
 	// }()
 
 	// 闭包 = 函数 + 外层变量的引用
-	r := a("百度") // r此时就是一个闭包
-	r()      // 相当于执行了a函数内部的匿名函数
+	// r := a("百度") // r此时就是一个闭包
+	// r()          // 相当于执行了a函数内部的匿名函数
+
+	// r := makeSuffixFunc(".txt")
+	// ret := r("上海")
+	// fmt.Println(ret)
+
+	x, y := calc(100)
+	ret1 := x(200) // base = 100 +200
+	fmt.Println(ret1)
+	ret2 := y(200)
+	fmt.Println(ret2)
+
 }
