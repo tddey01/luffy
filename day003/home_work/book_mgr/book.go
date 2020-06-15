4dc2783ad7605c6e2aa8ebce366f24b00ab65cd4
access <kevinlandun@gmail.com>
2020-06-16 00:46:14 +0800

    book
    Signed-off-by: access <kevinlandun@gmail.com>

--- day003/home_work/book_mgr/book.go
+++ day003/home_work/book_mgr/book.go
@@ -33,18 +33,8 @@ func inputBook() *Book {
 	return newBook
 }
 
-// // AddBook 是添加书籍的方法
-// func AddBook() {
-// 	newBook := inputBook()
-// 	for _, v := range AllBooks {
-// 		if v.title == newBook.title {
-// 			fmt.Printf("书名为%s的书已经存在！\n", newBook.title)
-// 			return
-// 		}
-// 	}
-// 	AllBooks := append(AllBooks, newBook)
-// }
 
+// AddBook 是添加书籍的方法
 func AddBook() {
 	newBook := inputBook()
 	for _, v := range AllBooks {
@@ -70,7 +60,7 @@ func ModifyBook() {
 	fmt.Printf("根据书名：%s无法查找到书籍信息！\n", newBook.title)
 }
 
-//showAllBook is the func of list all books.
+// ShowAllBook  is the func of list all books. 
 func ShowAllBook() {
 	for _, v := range AllBooks {
 		fmt.Printf("书名:《%s》 作者:%s 价格:%.2f 是否上架:%t\n", v.title, v.author, v.price, v.publish)
