320c764dbe2815e6a0c85e2176f0bb2e97584bac
access <kevinlandun@gmail.com>
2020-06-16 13:46:13 +0800

    继承类
    Signed-off-by: access <kevinlandun@gmail.com>

--- day003/16struct/main.go
+++ day003/16struct/main.go
@@ -7,14 +7,28 @@ type animal struct {
 	name string
 }
 
+// 定义一个动物会动的方法
 func (a *animal) move() {
-	fmt.Println("%s会动~")
+	fmt.Printf("%s 会动~\n", a.name)
 }
 
+//  定义一个狗的结构体
 type dog struct {
 	feet int
+	*animal
 }
 
+//  定义了一个狗的方法 wangwang
+func (d *dog) wangwang() {
+	fmt.Printf("%s 谁在: 旺旺~\n", d.name)
+}
 func main() {
-
+	var a = dog{
+		feet: 4,
+		animal: &animal{
+			name: "旺财",
+		},
+	}
+	a.wangwang() //调用狗的方法
+	a.move()     //调用动物的方法
 }
