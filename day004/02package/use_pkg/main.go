1e9890065fae60c150d73f3fa504a797ce101458
access <kevinlandun@gmail.com>
2020-06-16 18:29:54 +0800

    导入包起别名
    Signed-off-by: access <kevinlandun@gmail.com>

--- day004/02package/use_pkg/main.go
+++ day004/02package/use_pkg/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/tddey01/luffy/day004/02package/math_pkg"
+	m "github.com/tddey01/luffy/day004/02package/math_pkg"
 )
 
 const Mode = 1
 
 func main() {
-	math_pkg.Add(100, 200)
-	stu := math_pkg.Student{Name: "豪杰", Age: 19}
+	m.Add(100, 200)
+	stu := m.Student{Name: "豪杰", Age: 19}
 	fmt.Println(stu.Name)
 	fmt.Println(stu.Age)
 }
