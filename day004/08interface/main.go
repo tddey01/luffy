d6a07b239217b63690df95cf45681bc3b2bcd8b6
access <kevinlandun@gmail.com>
2020-06-18 12:45:22 +0800

    interface结构嵌套
    Signed-off-by: access <kevinlandun@gmail.com>

---
+++ day004/08interface/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+//  接口嵌套
+// 定义一个会说话的接口
+type speaker interface {
+	speak()
+}
+
+// 定义一个会移动的接口
+type mover interface {
+	move()
+}
+
+//接口嵌套
+
+type animal interface {
+	speaker
+	mover
+}
+
+type cat struct {
+	name string
+}
+
+func (c cat) speak() {
+	fmt.Println("喵喵喵")
+}
+
+func (c cat) move() {
+	fmt.Println("猫会东")
+}
+
+func main() {
+	var x animal
+	x = cat{name: "花花"}
+	x.move()
+	x.speak()
+}
