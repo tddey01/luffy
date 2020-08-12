4b3e564e1373e5a478eb58f1578d681f09451d76
kn <lau.liu@9street.org>
2020-08-12 17:55:39 +0800

    gopsutil
    Signed-off-by: kn <lau.liu@9street.org>

--- day015/gopsutil/main.go
+++ day015/gopsutil/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/shirou/gopsutil/net"
+
+	"github.com/shirou/gopsutil/disk"
+
 	"github.com/shirou/gopsutil/host"
 
 	"github.com/shirou/gopsutil/mem"
@@ -50,9 +54,57 @@ func getHostInfo() {
 	hInfo, _ := host.Info()
 	fmt.Printf("host info:%v uptime:%v boottime:%v\n", hInfo, hInfo.Uptime, hInfo.BootTime)
 }
+
+// 磁盘
+func getdisk() {
+	parts, err := disk.Partitions(true)
+	if err != nil {
+		fmt.Printf("get Partitions failed, err:%v\n", err)
+		return
+	}
+	//for _, part := range parts {
+	//	fmt.Printf("part:%v\n", part.String())
+	//	diskInfo, _ := disk.Usage(part.Mountpoint)
+	//	fmt.Printf("disk info:used:%v free:%v\n", diskInfo.UsedPercent, diskInfo.Free)
+	//}
+	//
+	//ioStat, _ := disk.IOCounters()
+	//for k, v := range ioStat {
+	//	fmt.Printf("%v:%v\n", k, v)
+	//}
+	fmt.Println(parts)
+	for _, part := range parts {
+		partInfo, err := disk.Usage(part.Mountpoint)
+		if err != nil {
+			fmt.Printf("get part stat failed,err:%v\n", err)
+			return
+		}
+		fmt.Println(partInfo)
+	}
+	ioStat, _ := disk.IOCounters()
+	for k, v := range ioStat {
+		fmt.Printf("%v:%v\n", k, v)
+	}
+}
+
+// 网络net IO
+func getnetworkinfo() {
+	info, err := net.IOCounters(true)
+	if err != nil {
+		fmt.Printf("get net io counters failed , err:%v\n", err)
+		return
+	}
+	fmt.Println(info)
+	for index, v := range info {
+		fmt.Printf("%v:%v send:%v recv:%v\n", index, v, v.BytesSent, v.BytesRecv)
+	}
+}
+
 func main() {
 	//getCpuInfo()
 	//getLoad()
 	//getMeminfo()
-	getHostInfo()
+	//getHostInfo()
+	//getdisk()
+	getnetworkinfo()
 }
